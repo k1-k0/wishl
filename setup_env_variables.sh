399f032295ccba88f98b0e455c2873c319338448
k1-k0 <kmssrvk@gmail.com>
2020-09-19 17:53:10 +0300

    create project structure

---
+++ setup_env_variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export WISHL_DB=
+export WISHL_DB_USER=
+export WISHL_DB_PASS=
+export WISHL_DB_HOST=
+export WISHL_DB_PORT=
---
+++ wishl/wishl_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WishlApiConfig(AppConfig):
+    name = 'wishl_api'
