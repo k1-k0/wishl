f89b25be9ab7fa48949303bbfe94d4d24da47bf3
k1-k0 <kmssrvk@gmail.com>
2020-10-03 00:46:49 +0300

    complete images api tests

--- wishl/wishl_api/tests/test_image_api.py
+++ wishl/wishl_api/tests/test_image_api.py
@@ -10,34 +10,49 @@ from ..views import ImageViewSet
 
 
 class ImageTests(APITestCase):
+    def setUp(self):
+        """
+        Prepare test data
+        """
+        self.image_1 = Image.objects.create(url='https://www.google.com/')
+        self.image_2 = Image.objects.create(url='https://www.facebook.com/')
 
 
-    def test_create_image_unauthorized(self):
-        """
-        Ensure that unauthorized user can't create image
-        """
-        url = reverse('image-list')
-        data = {'url': 'https://www.google.com/'}
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        self.assertEqual(Image.objects.count(), 0)
+        self.user = User.objects.create_user(username='test_u', password='secret1')
+        self.client.login(username='test_u', password='secret1')
 
     def test_create_image(self):
         """
         Ensure that user can create image
         """
-
-        user = User.objects.create_user(username='test_u', password='secret1')
-        self.client.login(username='test_u', password='secret1')
-
         url = reverse('image-list')
-        data = {'url': 'https://www.google.com/'}
+        data = {'url': 'https://www.gg.com/'}
         response = self.client.post(url, data, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Image.objects.count(), 1)
+        self.assertEqual(Image.objects.count(), 3)
 
     def test_edit_image_url(self):
-        pass
+        """
+        Ensure that user can edit image
+        """
+        url = '/images/4/'
+        data = {'url': 'https://www.fff.com/'}
+        response = self.client.put(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Image.objects.get(id=4).url, 'https://www.fff.com/')
+        self.assertEqual(Image.objects.count(), 2)
 
     def test_remove_image(self):
-        pass
+        """
+        Ensure that user can remove image
+        """
+        url = '/images/7/'
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Image.objects.count(), 1)
+
+        url = '/images/7/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
