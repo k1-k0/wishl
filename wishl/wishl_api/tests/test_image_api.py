49d95cf62aa15439d96f9a9f3fc1e01a7f287d5d
k1-k0 <kmssrvk@gmail.com>
2020-10-03 14:05:42 +0300

    remove unnecessary imports and formatting

--- wishl/wishl_api/tests/test_image_api.py
+++ wishl/wishl_api/tests/test_image_api.py
@@ -1,22 +1,20 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from rest_framework.test import APIRequestFactory
-from rest_framework.test import APIClient
-from rest_framework.test import force_authenticate
 from django.contrib.auth.models import User
 from ..models.image import Image
-from ..views import ImageViewSet
 
 
 class ImageTests(APITestCase):
+    """
+    Class for testing accessing to Image API
+    """
     def setUp(self):
         """
         Prepare test data
         """
         self.image_1 = Image.objects.create(url='https://www.google.com/')
         self.image_2 = Image.objects.create(url='https://www.facebook.com/')
-
 
         self.user = User.objects.create_user(username='test_u', password='secret1')
         self.client.login(username='test_u', password='secret1')
@@ -36,23 +34,24 @@ class ImageTests(APITestCase):
         """
         Ensure that user can edit image
         """
-        url = '/images/4/'
+        image = Image.objects.last()
+        url = f'/images/{image.id}/'
         data = {'url': 'https://www.fff.com/'}
         response = self.client.put(url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Image.objects.get(id=4).url, 'https://www.fff.com/')
+        self.assertEqual(Image.objects.get(id=image.id).url, 'https://www.fff.com/')
         self.assertEqual(Image.objects.count(), 2)
 
     def test_remove_image(self):
         """
         Ensure that user can remove image
         """
-        url = '/images/7/'
+        image = Image.objects.first()
+        url = f'/images/{image.id}/'
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Image.objects.count(), 1)
 
-        url = '/images/7/'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
